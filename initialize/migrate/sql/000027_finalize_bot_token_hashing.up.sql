cad4e514a66e7e277296be2a4064541c9afbcce7
luoshui <luoshuijs@outlook.com>
2026-03-14 23:54:38 +0800

    :bug: remove redundant bot token index drops
    Signed-off-by: luoshui <luoshuijs@outlook.com>

--- initialize/migrate/sql/000027_finalize_bot_token_hashing.up.sql
+++ initialize/migrate/sql/000027_finalize_bot_token_hashing.up.sql
@@ -7,8 +7,3 @@ ALTER TABLE bot_tokens
 ALTER TABLE bot_tokens
     DROP COLUMN access_token,
     DROP COLUMN refresh_token;
-
--- Remove old indexes that are no longer needed
-ALTER TABLE bot_tokens
-    DROP INDEX uniq_access_token,
-    DROP INDEX uniq_refresh_token;
